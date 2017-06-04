75b1d5e18bab290f1113cc8a56b677966035bc64
Paolo <dino.tartaro@gmail.com>
2017-06-04 03:28:57 +0200

    Add files via upload
    Displays the signal quality and the connection to adjust the wifi antenna

---
+++ wifi-reg.sh
@@ -0,0 +1,70 @@
+#!/bin/bash
+#
+#  		wifi-reg.sh
+#  
+#  Copyright 2017 c.realkiller <dino.tartaro@gmail.com>
+#  
+#
+#  This program is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation; either version 2 of the License, or
+#  (at your option) any later version.
+#  
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#  
+#  You should have received a copy of the GNU General Public License
+#  along with this program; if not, write to the Free Software
+#  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+#  MA 02110-1301, USA.
+#  
+#Displays the signal quality and the connection to adjust the wifi antenna
+#for cantenna regulations (prinless tube ;-) )
+
+lname=`basename $0 | cut -d '.' -f 1`
+echo ${lname^^}
+if [ "$(id -u)" != "0" ]; then
+	sudo -k
+	wl=`sudo -S iwgetid | cut -d ' ' -f 1`
+		if sudo -n true 2>/dev/null; then 
+			while (true) 
+			do
+				clear
+				echo ${lname^^}
+				echo
+				sudo -S iwconfig $wl | grep -o -e 'wlan1' -o -e 'Link Quality=[^*]*' | cut -d '=' -f1,2,3,4
+				echo
+				echo "CTRL+C to stop this script"
+				sleep 1
+
+			clear
+			done
+			else
+			echo "chiudo lo script"
+			sleep 3
+			exit 1
+		fi
+	else
+
+	wl=`iwgetid | cut -d ' ' -f 1`
+
+	while (true)
+	do 
+		clear
+		echo ${lname^^}
+		echo
+		iwconfig $wl | grep -o -e 'wlan1' -o -e 'Link Quality=[^*]*' | cut -d '=' -f1,2,3,4
+		echo
+		echo "CTRL+C to stop this script"
+		sleep 1
+
+	clear
+
+	done
+fi	
+exit 0
+
+
+
